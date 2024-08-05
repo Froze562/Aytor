43f14a82539f025e0c9e4eb483dbb9978342f27f
Froze562 <159714409+Froze562@users.noreply.github.com>
2024-08-05 08:48:31 +0600

    Add files via upload

---
+++ Assets/Scripts/script.js
@@ -0,0 +1,56 @@
+// Navbar_Menu_Btn
+let nav_btn = document.querySelector(".nav-btn");
+let sideMenu = document.querySelector(".side-menu");
+
+nav_btn.onclick = function(){
+     nav_btn.classList.toggle("active")
+     sideMenu.classList.toggle("active")
+};
+document.onclick = function(e){
+     if (!nav_btn.contains(e.target) && !sideMenu.contains(e.target)){
+          nav_btn.classList.remove("active")
+          sideMenu.classList.remove("active")
+     }
+};
+
+// Nav_Function
+let searchbox_open_icon = document.querySelector(".searchbox-open-icon");
+let searchbox_close_icon = document.querySelector(".searchbox-close-icon");
+let searchbox_search_icon = document.querySelector(".searchbox-search-icon");
+let search_box = document.querySelector(".search-box");
+let search_bar = document.querySelector(".search-bar");
+
+searchbox_open_icon.addEventListener('click', function(){
+     search_bar.classList.add("active")
+     search_box.classList.add("active")
+});
+search_bar.addEventListener('click', function(){
+     search_bar.classList.remove("active")
+     search_box.classList.remove("active")
+});
+searchbox_close_icon.addEventListener('click', function(){
+     search_bar.classList.remove("active")
+     search_box.classList.remove("active")
+});
+searchbox_search_icon.addEventListener('click', function(){
+     search_bar.classList.remove("active")
+     search_box.classList.remove("active")
+});
+
+// Offer-layer
+$('.offer-layer').slick({
+     arrows: false,
+     dots: true,
+     speed: 2500,
+     infinite: true,
+     autoplay: true,
+     slidesToShow: 1,
+});
+$('.offer-layer-responsive').slick({
+     arrows: false,
+     dots: true,
+     speed: 2500,
+     infinite: true,
+     autoplay: true,
+     slidesToShow: 1,
+});
