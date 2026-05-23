be5fd45a250723b0a7108ff83350ec973043098e
Froze562 <frozenflame351@gmail.com>
2026-05-23 22:50:11 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-semibold text-indigo-500 h-screen bg-amber-100">
+        Thanks WebX Learner, For saving my time. New Jorney Begins from July.
+      </h1>
+    </>
+  );
+}
+
+export default App;
